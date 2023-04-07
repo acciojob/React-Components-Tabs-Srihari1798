147a5cc8294208bc0343d8d3b1431a5607484fa8
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-07 22:46:14 +0530

    initial commit

---
+++ src/components/Tab.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Tab() {
+  return (
+    <div>
+      <div className='tabs'>
+        <div className='tab' id="Home-tab">Home</div>
+        <div className='tab' id='About-tab'>About</div>
+        <div className='tab' id='Features-tab'>Features</div>
+        <div className=' viewport'> Pages Go Here .</div>
+      </div>
+    </div>
+  )
+}
