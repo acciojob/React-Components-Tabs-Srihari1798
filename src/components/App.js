900035af1a544c4fc55890abaec389aa1deef8c3
Srihari1798 <120098197+Srihari1798@users.noreply.github.com>
2023-04-17 22:27:38 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Tab from './Tab'
+
+const App = () => {
+  return (
+    <div>
+       <Tab/>
+    </div>
+  )
+}
+
+export default App
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -7,7 +7,7 @@ export default function Tab() {
         <div className='tab' id="Home-tab">Home</div>
         <div className='tab' id='About-tab'>About</div>
         <div className='tab' id='Features-tab'>Features</div>
-        <div className=' viewport'> Pages Go Here .</div>
+        <div className='viewport'> Pages Go Here .</div>
       </div>
     </div>
   )
